481c7324ca3d52859c6f9afb90e6f5955e7b2189
eludelu123 <150374783+eludelu123@users.noreply.github.com>
2023-11-09 14:13:34 -0300

    Update site.js

--- site.js
+++ site.js
@@ -1,14 +1,14 @@
 slide1();
 
 function slide1(){
-    document.getElementById('imag').src="img/frango.jpeg";
+    document.getElementById('imag').src="frango.png";
     setTimeout("slide2()", 1000)
     }
     function slide2(){
-    document.getElementById('imag').src="img/calabresa.png";
+    document.getElementById('imag').src="calabresa.png";
     setTimeout("slide3()", 1000)
     }
     function slide3(){
-    document.getElementById('imag').src="teste3.jpg";
+    document.getElementById('imag').src="fundo.jpg";
     setTimeout("slide1()", 1000)
     }
